269857d6a11091aac150f1b128f4587309ea5f45
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2021-10-13 19:59:23 -0400

    Update im_encrypt.py

--- im_encrypt.py
+++ im_encrypt.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 image = plt.imread('image.tiff')[:,:,:3]
 # plt.imshow(image)
@@ -7,3 +8,14 @@ g_row=[]
 for row in image:
     for pixel in row:
         g_row.append(.2126*pixel[0] + .7152*pixel[1] + .0722*pixel[2])
+
+
+
+def float64_uint8(img):
+    #gets the max number the data could be
+    img_data_max = np.iinfo(img.dtype).max
+    img = img.astype(np.float64)/img_data_max
+    img = 255*img
+    img = img.astype(np.uint8)
+
+    return img

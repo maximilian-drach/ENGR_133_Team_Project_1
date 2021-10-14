2eff2f6a4f425363026fecffd918aefac5fd5c68
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2021-10-13 21:24:32 -0400

    Create im_encrypt_works.py

---
+++ im_encrypt_works.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gray(image):
+
+    gray_values = (image[:,:,0]*.299 + image[:,:,1]*.587 + image[:,:,2]*.114)
+    grey = image.copy()
+    for i in range(3):
+        grey[:,:,i] = gray_values
+
+    return grey
+    
+
+def float64_uint8(img):
+    #gets the max number the data could be
+    img_data_max = np.iinfo(img.dtype).max
+    img = img.astype(np.float64)/img_data_max
+    img = 255*img
+    img = img.astype(np.uint8)
+
+    return img
+def uint8_float64(img):
+    img_data_max = np.iinfo(img.dtype).max
+    img = img.astype(np.unit8)/img_data_max
+    img = img.astype(np.float64)
+
+    return img
+
+image = plt.imread('image.tiff')[:,:,:3]
+image = gray(image)
+plt.imshow(image)

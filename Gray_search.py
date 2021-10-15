5db63ba9f2044012fa0b89e17e59286d0a78e753
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2021-10-14 20:52:19 -0400

    swtiched gray to grey

--- Gray_search.py
+++ Gray_search.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def gray(image):
+def grey(image):
 
-    gray_values = (image[:,:,0]*.299 + image[:,:,1]*.587 + image[:,:,2]*.114)
+    grey_values = (image[:,:,0]*.299 + image[:,:,1]*.587 + image[:,:,2]*.114)
     grey = image.copy()
     for i in range(3):
-        grey[:,:,i] = gray_values
+        grey[:,:,i] = grey_values
 
     return grey
-    
+
+
+def earth_finder(grey_image):
+    grey
+
+
 
 def float64_uint8(img):
     #gets the max number the data could be
@@ -19,6 +24,8 @@ def float64_uint8(img):
     img = img.astype(np.uint8)
 
     return img
+
+
 def uint8_float64(img):
     img_data_max = np.iinfo(img.dtype).max
     img = img.astype(np.unit8)/img_data_max
@@ -29,5 +36,5 @@ def uint8_float64(img):
 
 
 image = plt.imread('image.tiff')[:,:,:3]
-image = gray(image)
+image = grey(image)
 plt.imsave('gray.tiff', image)

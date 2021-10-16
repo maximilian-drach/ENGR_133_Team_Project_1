4e29fc47a6b6f70fdfe7ae2741c2e6ce29bcedca
vaculatearc003 <limaye@purdue.edu>
2021-10-16 15:45:29 -0400

    wa

--- Histo_Key.py
+++ Histo_Key.py
@@ -8,15 +8,16 @@ Created on Wed Oct 13 19:41:56 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-image=plt.imread('Pale_Blue_Dot_Encrypted.tiff')
+image=plt.imread('Pale_Blue_Dot_Encrypted.tiff')[:,:,:3]
 def histogram(image):
     image=(image*255).astype(np.uint8)
     
-    plt.hist(image[:,:,0].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1))
-    plt.hist(image[:,:,1].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1))
-    plt.hist(image[:,:,2].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1))
-
+    plt.hist(image[:,:,0].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1), color='red', alpha=.5, label='Red Pixels')
+    plt.hist(image[:,:,1].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1), color='green', alpha=.5, label='Green Pixels')
+    plt.hist(image[:,:,2].reshape(image.shape[0]*image.shape[1]),bins=np.arange(2**8+1), color='blue', alpha=.5, label='Blue Pixels')
+    plt.legend()
+ 
 
 
 def new_key(phrase):
-    w
+    

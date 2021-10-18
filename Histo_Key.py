f95710bdbd1b8ec319e3c0efe94a538fd420bcce
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2021-10-18 12:58:48 -0400

    Update Histo_Key.py

--- Histo_Key.py
+++ Histo_Key.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 13 19:41:56 2021
 
-@author: ashli
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
+import key_generator_team07 as kg
 
 image=plt.imread('Pale_Blue_Dot_Encrypted.tiff')[:,:,:3]
 def histogram(image):
@@ -21,5 +17,77 @@ def histogram(image):
  
 histogram(image)   
 
-def new_key(phrase):
+def better_key(image, phrase):
+    phrase = phrase.replace(' ', '')
+    len_phrase = len(phrase)
+    
+    np.random.seed(len_phrase)
+    
+    img = plt.imread(image)[:,:,:3]
+    row = img.shape[0]
+    col = img.shape[1]
+    
+    
+    key = np.zeros([row,col], dtype=np.uint8)
+    
+    for r in range(row):
+        for c in range(col):
+            key[r][c] = np.random.randint(0, high=266, size=None, dtype=int)
+    
+    return key
+
+            
+    # 1. get the length of the phrase
+    # 2. put the lenght into the rand seed
+    # 3. make the seed vales form 0-255
+    # 4. Xor the image
+    # 5. that encryotuion and do xor to decrypt
+    
+
+# def seed_func(si, s0):
+    
+# def use_key(image, phrase):
+#     phrase = phrase.replace(' ', '')
+#     len_phrase = len(phrase)
+    
+#     seed = len_phrase
+    
+#     img = plt.imread(image)[:,:,:3]
+#     row = img.shape[0]
+#     col = img.shape[1]
+    
+    
+#     key = np.zeros([row,col], dtype=np.uint8)
+    
+#     for r in range(row):
+#         for c in range(col):
+            
+    
+    
+    
+    
+    
+#image = plt.imread('Pale_Blue_Dot_Encrypted.tiff')[:,:,:3]
+#key = kg.key_generator(image, 'COME AND GET YOUR LOVE')
+#pic = kg.XOR_Cypher(img, key)
+
+
+image = 'image.tiff'
+
+nKey = better_key(image, 'Test')
+pic = kg.XOR_Cypher(image, nKey)
+
+plt.imsave('encypt.tiff', pic)
+
+
+
+
+
+encrypted = 'encypt.tiff'
+
+nKey = better_key(encrypted, 'Test')
+image2 = kg.XOR_Cypher(encrypted, nKey)
+
+plt.imsave('orgin.tiff', image2)
+
     

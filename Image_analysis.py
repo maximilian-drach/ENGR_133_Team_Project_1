8853e87b918381100934510e535db6b6b18d5bc6
maximilian-drach <80358780+maximilian-drach@users.noreply.github.com>
2021-10-19 12:21:48 -0400

    n

--- Image_analysis.py
+++ Image_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img= input('Enter image file name: ')
+
 def image_tester(img):
     if img.endswith('.jpg'):
         image = plt.imread(img)
@@ -11,11 +12,12 @@ def image_tester(img):
     elif img.endswith('.tiff'):
         image = plt.imread(img)[:,:,:3]
  
-
+    return image
    
 image = plt.imread(img)
-print(f'Dimensions of the image is {image.shape}') 
-plt.imshow(image)
+def dim(image):
+    print(f'Dimensions of the image is {image.shape}') 
+    plt.imshow(image)
 
 print(image)
     
